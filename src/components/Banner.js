b9d8290bb3782938c5bfad88cb3fdccefa69a6fb
LottaMonet <lotta@anniecannons.com>
2026-04-09 14:56:17 -0700

    update banner.js
    take 2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,6 +15,7 @@ export const Banner = () => {
   const toRotate = ["Software Engineer", "Mother", "Fitness Lover"];
   const period = 2000;
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     const ticker = setInterval(() => {
       tick();
@@ -26,22 +27,23 @@ export const Banner = () => {
   const tick = () => {
     const i = loopNum % toRotate.length;
     const fullText = toRotate[i];
+  
     const updatedText = isDeleting
       ? fullText.substring(0, text.length - 1)
       : fullText.substring(0, text.length + 1);
-
+  
     setText(updatedText);
-
+  
     if (isDeleting) {
-      setDelta((prevDelta) => prevDelta / 2);
+      setDelta((prev) => prev / 2);
     }
-
+  
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
       setDelta(period);
     } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
-      setLoopNum((prevLoopNum) => prevLoopNum + 1);
+      setLoopNum((prev) => prev + 1);
       setDelta(500);
     }
   };
